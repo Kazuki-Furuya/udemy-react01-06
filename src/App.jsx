702af8bc938c13dac5005d6e335fb72547c0e3ff
kazuki-furuya <furuyakazuki@gmail.com>
2023-02-04 22:22:12 +0900

    5-49

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+// esrint react-hooks/exhaustive-deps:off
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 
 const App = () => {
-  const onClickButton = () => alert();
+  console.log("start rendering");
+  const [num, setNum] = useState(2);
+  const [faceShowFlag, setFaceShowFlag] = useState(true);
+
+  const onClickcountUp = () => {
+    setNum(num + 1);
+  };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlag(!faceShowFlag);
+  };
+
+  useEffect(() => {
+    if (num % 3 === 0) {
+      faceShowFlag || setFaceShowFlag(true);
+    } else {
+      faceShowFlag && setFaceShowFlag(false);
+    }
+  }, [num]);
+
   return (
     <>
       <h1 style={{ color: "red" }}>hello!</h1>
       <ColorfulMessage color="blue">お元気ですか？</ColorfulMessage>
       <ColorfulMessage color="black">fine!</ColorfulMessage>
 
-      <button onClick={onClickButton}>button</button>
+      <button onClick={onClickcountUp}>countUp</button>
+      <br />
+      <button onClick={onClickSwitchShowFlag}>on/off</button>
+      <p>{num}</p>
+      {/* { num % 3 === 0 && <p>😲</p>} */}
+      {faceShowFlag && <p>😲</p>}
     </>
   );
 };
